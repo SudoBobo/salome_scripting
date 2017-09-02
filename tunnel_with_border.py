7cd97192e8dffd2a76bba27f99e9880bd631a03f
SudoBobo <ivan.koptelov@phystech.edu>
2017-09-02 16:53:19 +0300

    make border rock-like

--- tunnel_with_border.py
+++ tunnel_with_border.py
@@ -3,7 +3,7 @@
 ###
 ### This file is generated automatically by SALOME v8.2.0 with dump python functionality
 ###
-
+import random
 import sys
 import salome
 
@@ -25,7 +25,9 @@ import math
 import SALOMEDS
 
 
-def create_border(x_0, y_0, x_fin, y_fin, step, border_vertexes, border_lines):
+def create_border(x_0, y_0, x_fin, y_fin, step, border_vertexes, border_lines, coef):
+
+
     # add ALL border dots (r_o and r_fin)
     k = (y_fin - y_0) / (x_fin - x_0)
     a = y_0 - k * x_0
@@ -36,10 +38,10 @@ def create_border(x_0, y_0, x_fin, y_fin, step, border_vertexes, border_lines):
     prev_vertex = geompy.MakeVertex(x_0, y_0, 0)
     border_vertexes.append(prev_vertex)
 
-    while x < x_fin and y > y_fin:
+    while x < x_fin - step and y > y_fin :
 
         x = x + step
-        y = k * x + a
+        y = k * x + a + random.random() * coef * step
 
         new_vertex = geompy.MakeVertex(x, y, 0)
         border_vertexes.append(new_vertex)
@@ -48,10 +50,10 @@ def create_border(x_0, y_0, x_fin, y_fin, step, border_vertexes, border_lines):
 
         prev_vertex = new_vertex
 
-    # final_vertex = geompy.MakeVertex(x_fin, y_fin, 0)
-    # border_vertexes.append(final_vertex)
-    #
-    # border_lines.append(geompy.MakeLineTwoPnt(prev_vertex, final_vertex))
+    final_vertex = geompy.MakeVertex(x_fin, y_fin, 0)
+    border_vertexes.append(final_vertex)
+    border_lines.append(geompy.MakeLineTwoPnt(prev_vertex, final_vertex))
+
 
 geompy = geomBuilder.New(theStudy)
 
@@ -83,7 +85,8 @@ border_vertexes = list()
 border_lines = list()
 step = 5
 
-create_border(x_0, y_0, x_fin, y_fin, step, border_vertexes, border_lines)
+coef = 4
+create_border(x_0, y_0, x_fin, y_fin, step, border_vertexes, border_lines, coef)
 
 # tunnel created
 tunnel_vertexes = [geompy.MakeVertex(-sc, -sc, 0), geompy.MakeVertex(0, sc, 0), geompy.MakeVertex(sc, -sc, 0)]
@@ -97,22 +100,22 @@ triangle_vertexes = [geompy.MakeVertex(0, 20*sc, 0), geompy.MakeVertex(20*sc, 20
 triangle_lines = [geompy.MakeLineTwoPnt(triangle_vertexes[0], triangle_vertexes[1]),
                   geompy.MakeLineTwoPnt(triangle_vertexes[1], triangle_vertexes[2])]
 
-triangle_border_vertexes = list()
-triangle_border_lines = list()
+triangle_border_vertexes = list(border_vertexes)
+triangle_border_lines = list(border_lines)
 
 # create copy of border for second (triangle) face
-create_border(x_0, y_0, x_fin, y_fin, step, triangle_border_vertexes, triangle_border_lines)
+# create_border(x_0, y_0, x_fin, y_fin, step, triangle_border_vertexes, triangle_border_lines)
 
 
 # create face_1 (don't forget to cut out tunnel), face_2, compound
-# tunnel_face = geompy.MakeFaceWires([tunnel_arc, tunnel_line], 1)
-# trap_face = geompy.MakeFaceWires(trap_lines + border_lines, 1)
+tunnel_face = geompy.MakeFaceWires([tunnel_arc, tunnel_line], 1)
+trap_face = geompy.MakeFaceWires(trap_lines + border_lines, 1)
 #
-# cut_trap_face = geompy.MakeCutList(trap_face, [tunnel_face], True)
+cut_trap_face = geompy.MakeCutList(trap_face, [tunnel_face], True)
 #
-# triangle_face = geompy.MakeFaceWires(triangle_lines + triangle_border_lines, 1)
+triangle_face = geompy.MakeFaceWires(triangle_lines + triangle_border_lines, 1)
 #
-# Compound = geompy.MakeCompound([cut_trap_face, triangle_face])
+Compound = geompy.MakeCompound([cut_trap_face, triangle_face])
 
 geompy.addToStudy(O, 'O')
 geompy.addToStudy(OX, 'OX')
@@ -134,11 +137,11 @@ for a in range(len(all_arcs)):
     geompy.addToStudy(all_arcs[a], 'Arc_{}'.format(a))
 
 
-# geompy.addToStudy(tunnel_face, 'tunnel_face')
-# geompy.addToStudy(trap_face, 'trap_face')
-# geompy.addToStudy(cut_trap_face, 'cut_trap_face')
-# geompy.addToStudy(triangle_face, 'triangle_face')
-# geompy.addToStudy(Compound, 'Compound')
+geompy.addToStudy(tunnel_face, 'tunnel_face')
+geompy.addToStudy(trap_face, 'trap_face')
+geompy.addToStudy(cut_trap_face, 'cut_trap_face')
+geompy.addToStudy(triangle_face, 'triangle_face')
+geompy.addToStudy(Compound, 'Compound')
 
 if salome.sg.hasDesktop():
     salome.sg.updateObjBrowser(True)
